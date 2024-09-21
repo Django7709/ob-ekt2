aa0482faa402970885d0338d5cf57ec00269b6ca
django <pitbull.09@mail.ru>
2024-09-21 07:42:10 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,90 @@
+import java.util.Objects;
+
+public class Author {
+    private String name;
+    private String lastName;
+
+    public Author(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+    return this.name;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(lastName, author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Автор книги - " + getName() + " " + getLastName();
+    }
+
+}
+import java.util.Objects;
+
+public class Book {
+    private String nameBooks;
+    private int agePublication;
+    private Author author;
+
+    public Book(String nameBooks, Author author, int agePublication) {
+
+        this.nameBooks = nameBooks;
+        this.agePublication = agePublication;
+        this.author = author;
+    }
+
+    public int getAgePublication() {
+        return this.agePublication;
+    }
+
+    public String getNameBook() {
+        return this.nameBooks;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public void setAgePublication(int agePublication) {
+        this.agePublication = agePublication;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return agePublication == book.agePublication && Objects.equals(nameBooks, book.nameBooks) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(agePublication, nameBooks, author);
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги - " + getNameBook() + "\n" + getAuthor() + "\nГод публикации книги - " + getAgePublication();
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         printSeparator(); //Вывод разделителя
